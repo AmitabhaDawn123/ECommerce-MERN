aa1ad391c590bdc983bb7b1a03e8b7e86dec6852
AmitabhaDawn123 <amitabhadawn73@gmail.com>
2024-03-28 15:01:35 +0530

    created express app and connected all files

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv"
+
+import express from "express"
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+dotenv.config({ path: "server/config/config.env" });
+
+app.get('/cart', (req, res) => {
+    res.send('I Love You Babu')
+  })
+
+app.listen(PORT,()=>{
+    console.log(`🌍 Now listening on PORT:${PORT}`);
+})
+
+
+console.log("chikushi"); 
